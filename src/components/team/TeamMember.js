b1f8553edc8980d637f65481e97a92ec3f453289
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2024-07-13 01:16:43 -0400

    added the rest of the team

--- src/components/team/TeamMember.js
+++ src/components/team/TeamMember.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './TeamMember.css';
 
-const TeamMember = ({ name, role, image }) => {
+const TeamMember = ({ name, role, image, isFirstMember }) => {
   const roleLines = role.split('\n');
   return (
-    <div className="team-member">
-      <img src={image} alt={`${name}'s profile`} />
+    <div className={`team-member ${isFirstMember ? 'first-member' : ''}`}>
+      <img src={image} alt={`${name}'s profile`} className={isFirstMember ? 'first-member-image' : ''} />
       <h3>{name}</h3>
       <p>
         {roleLines.map((line, index) => (
