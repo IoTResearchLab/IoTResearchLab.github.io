d6be5a2bf09374eca7a86253f2fa8f4a07516c53
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2024-07-13 03:41:58 -0400

    removed all duplicated content form field project pages

--- src/components/healthpage/index.js
+++ src/components/healthpage/index.js
@@ -5,41 +5,11 @@ import styles from './styles.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const FeatureList = [
-  {
-    title: 'IOT in Health Care',
-    link: '/health', // Update this path
-    imgSrc: '/img/health.webp',
-    description: (
-      <>
-        Healthcare is shifting towards preventive care, but heart care management is overlooked by both wellness and MedTech companies. With widespread digital connectivity, there's a significant opportunity for connected health devices and remote patient monitoring to fill this gap. This can enhance heart care management and integrate it into the broader preventive care landscape.
-      </>
-    ),
-  },
-  {
-    title: 'IoT-driven Traffic Management: Enhancing Urban Mobility with Smart Solutions',
-    link: '/traffic', // Update this path
-    imgSrc: '/img/smart-city.webp', // Use relative path here
-    description: (
-      <>
-        Addresses the challenges of maintaining user privacy amidst the proliferation of interconnected devices, emphasizing strategies and technologies to secure personal data and communications in IoT environments.
-      </>
-    ),
-  },
-  {
-    title: 'IoT Infrastructure and Applications',
-    link: '/infra', // Update this path
-    imgSrc: '/img/infra.webp',
-    description: (
-      <>
-        Involves creating foundational networks and frameworks to support interconnected IoT devices and applications, ensuring reliable connectivity and efficient data management across various domains like smart cities, healthcare, and agriculture.
-      </>
-    ),
-  },
+  
 ];
 
-function Feature({ Svg, imgSrc, title, description, link }) {
+function Feature({ Svg, imgSrc, title, description }) {
   const resolvedImgSrc = useBaseUrl(imgSrc);
-  const resolvedLink = useBaseUrl(link);
 
   return (
     <div className={clsx('col col--4')}>
@@ -51,20 +21,19 @@ function Feature({ Svg, imgSrc, title, description, link }) {
         ) : null}
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">
-          <a href={resolvedLink}>{title}</a>
-        </Heading>
+        <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
     </div>
   );
 }
 
-export default function HealthPage() {
+
+export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <h1 className={styles.text}>Research Fields</h1>
+        <h1 className={styles.text}>Field Projects</h1>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
--- src/components/trafficpage/index.js
+++ src/components/trafficpage/index.js
@@ -5,32 +5,7 @@ import styles from './styles.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const FeatureList = [
-  {
-    title: <a href="docs/intro">IOT in Health Care</a>,
-    imgSrc: '/img/health.webp',
-    description: (
-      <>
-Healthcare is shifting towards preventive care, but heart care management is overlooked by both wellness and MedTech companies. With widespread digital connectivity, there's a significant opportunity for connected health devices and remote patient monitoring to fill this gap. This can enhance heart care management and integrate it into the broader preventive care landscape.      </>
-    ),
-  },
-  {
-    title: <a href="docs/intro">IoT-driven Traffic Management: Enhancing Urban Mobility with Smart Solutions</a>,
-    imgSrc: '/img/traffic.webp', // Use relative path here
-    description: (
-      <>
-        Addresses the challenges of maintaining user privacy amidst the proliferation of interconnected devices, emphasizing strategies and technologies to secure personal data and communications in IoT environments.
-      </>
-    ),
-  },
-  {
-    title: <a href="docs/intro">IoT Infrastructure and Applications</a>,
-    imgSrc: '/img/infra.webp',
-    description: (
-      <>
-        Involves creating foundational networks and frameworks to support interconnected IoT devices and applications, ensuring reliable connectivity and efficient data management across various domains like smart cities, healthcare, and agriculture.
-      </>
-    ),
-  },
+
 ];
 
 function Feature({ Svg, imgSrc, title, description }) {
@@ -58,7 +33,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <h1 className={styles.text}>Research Fields</h1>
+        <h1 className={styles.text}>Field Projects</h1>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
