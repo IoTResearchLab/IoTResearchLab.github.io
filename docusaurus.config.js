bbf872145b0e76051a2b2e56706fcaa85ceb4a7e
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2024-07-16 19:23:28 -0400

    changed the search bar position

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,10 +74,11 @@ const config = {
           { to: '/team', label: 'Our Team', position: 'left' },
           { to: '/Publications', label: 'Publications', position: 'left' },
           { to: '/traffic', label: 'Smart Mobility', position: 'left' },
+          { type: 'search', position: 'right' }, 
           { to: '/join-us', label: 'Contact Us', position: 'right' },
           { to: '/health', label: 'IoT in Health Care', position: 'left' },
           { to: '/infra', label: 'IoT Infrastructure', position: 'left' },
-          { type: 'search', position: 'right' }, 
+       
         ],
       },
       footer: {
