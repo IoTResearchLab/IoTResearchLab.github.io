ad596f2d61920e159ed699db57a006edad843385
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2024-07-31 01:16:34 -0400

    changed the projectpage format

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,13 +1,12 @@
 // src/pages/ProjectPage.jsx
 import React from 'react';
 import Layout from '@theme/Layout';
-import './ProjectPage.css'; // Import the stylesheet
+import './ProjectPage.css'; 
 
 const ProjectPage = (props) => {
   const { customData } = props;
 
-  console.log('customData:', customData); // Debug log
-
+  console.log('customData:', customData);
   if (!customData) {
     return <div>Loading...</div>;
   }
@@ -15,30 +14,37 @@ const ProjectPage = (props) => {
   const { title, subtitle, introduction, paragraphs, publications } = customData;
 
   return (
+    <main>
     <Layout title={title}>
-      <div className="container">
+      <div className="project-page-container">
         <h1>{title}</h1>
         {subtitle && <h2>{subtitle}</h2>}
         <p>{introduction}</p>
+        <hr style={{ margin: '2rem 0' }} />
+
         {paragraphs.map((paragraph, index) => (
           <div key={index}>
-            {paragraph.title && <h3>{paragraph.title}</h3>}
-            <p>{paragraph.paragraph}</p>
-            {paragraph.img && <img src={paragraph.img} alt={paragraph.title} style={{ maxWidth: '100%' }} />}
+              {paragraph.title && <h3>{paragraph.title}</h3>}
+            <div className="paragraph-container">
+              {paragraph.img && <img src={paragraph.img} alt={paragraph.title} />}
+              <p>{paragraph.paragraph}</p>
+            </div>
             <hr style={{ margin: '2rem 0' }} />
           </div>
         ))}
         {publications.length > 0 && (
           <div>
-            <h2>Publications</h2>
+            <h3>Publications</h3>
             <ul>
               {publications.map((publication, index) => (
                 <li key={index}>
                   <a href={publication.url} target="_blank" rel="noopener noreferrer">
                     {publication.title}
                   </a>
-                  <p>Authors: {publication.authors}</p>
-                  <p>Date: {publication.date}</p>
+                  <br/>
+                  <pub1>{publication.authors}</pub1>
+                  <br/>
+                  <publ>{publication.date}</publ>
                 </li>
               ))}
             </ul>
@@ -46,7 +52,18 @@ const ProjectPage = (props) => {
         )}
       </div>
     </Layout>
+    </main>
   );
 };
 
+
+
 export default ProjectPage;
+/* {paragraphs.map((paragraph, index) => (
+          <div key={index}>
+            {paragraph.title && <h3>{paragraph.title}</h3>}
+            <p>{paragraph.paragraph}</p>
+            {paragraph.img && <img src={paragraph.img} alt={paragraph.title} style={{ maxWidth: '100%' }} />}
+            <hr style={{ margin: '2rem 0' }} />
+          </div>
+        ))}*/
